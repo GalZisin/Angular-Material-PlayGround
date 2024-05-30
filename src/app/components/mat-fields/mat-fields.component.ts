1b35e696205c065e3cd45ff6da97e1ef7319a73c
Gal Zisin <46171488+GalZisin@users.noreply.github.com>
2024-05-30 22:08:01 +0300

    add rows

--- src/app/components/mat-fields/mat-fields.component.ts
+++ src/app/components/mat-fields/mat-fields.component.ts
@@ -3,7 +3,9 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, NativeDateAdapter } from '@angular/material/core';
 @Component({
   selector: 'app-mat-fields',
   standalone: true,
@@ -12,7 +14,12 @@ import { MatRadioModule } from '@angular/material/radio';
     ReactiveFormsModule,
     MatSelectModule,
     MatInputModule,
-    MatRadioModule
+    MatRadioModule,
+    MatDatepickerModule,
+    MatFormFieldModule
+  ],
+  providers: [{ provide: DateAdapter, useClass: NativeDateAdapter },
+  { provide: MAT_DATE_FORMATS, useValue: MAT_NATIVE_DATE_FORMATS }
   ],
   templateUrl: './mat-fields.component.html',
   styleUrl: './mat-fields.component.scss'
